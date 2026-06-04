9c190b707819f92af6444e6940c598c745ad040d
sod1qovamaftuna0524-cyber <sod1qovamaftuna0524@gmail.com>
2026-06-04 15:21:19 +0500

    settings.py

--- news_project/settings.py
+++ news_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5%bkg1zg%)2r)2o&5(bzdnkiyk_b&@2i1lb_%829kx@)0&-!()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['uznews-search.onrender.com', '*']
 
 
 # Application definition
